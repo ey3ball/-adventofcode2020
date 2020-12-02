c150fb675016ae599944abf0c1e699f8d86501b0
Florian Vallée <florian@vallee.xyz>
2020-12-02 12:53:34 +0100

    day02: solved part2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -13,7 +13,7 @@ fn parse<'a>(input: &'a str) -> (usize, usize, char, &'a str) {
     (freq_min, freq_max, c, password)
 }
 
-fn main() {
+fn day01_1() {
     let input = "input/input.txt";
     let contents = fs::read_to_string(input).unwrap();
 
@@ -24,7 +24,7 @@ fn main() {
     let valid_passwords = passwords.iter().filter(
         |(min, max, c, password)| {
             let counter = password.chars().collect::<Counter<_>>();
-            println!("{:#?} {:#?} {:#?}", password, c, counter[c]);
+            //println!("{:#?} {:#?} {:#?}", password, c, counter[c]);
 
             if counter[c] >= *min && counter[c] <= *max {
                 true
@@ -34,4 +34,30 @@ fn main() {
     }).count();
 
     println!("{:#?}", valid_passwords);
+}
+
+fn day01_2() {
+    let input = "input/input.txt";
+    let contents = fs::read_to_string(input).unwrap();
+
+    let passwords: Vec<(usize, usize, char, &str)> = contents.lines().map(
+        |x| parse(x)).collect();
+
+
+    let valid_passwords = passwords.iter().filter(
+        |(p1, p2, c, password)| {
+            let chars: Vec<char> = password.chars().collect();
+            let pos1 = *p1 - 1;
+            let pos2 = *p2 - 1;
+            (chars[pos1] == *c || chars[pos2] == *c)
+                && chars[pos1] != chars[pos2]
+    }).count();
+
+    println!("{:#?}", valid_passwords);
+}
+
+
+fn main() {
+    day01_1();
+    day01_2();
 }
