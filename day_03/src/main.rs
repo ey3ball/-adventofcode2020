96f95111b22e96672da67b3ea5d909177f806720
Florian Vallée <florian@vallee.xyz>
2020-12-03 09:19:05 +0100

    day03: part2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,29 +1,54 @@
 use std::fs;
 
-fn day_03_1() {
+fn parse_input() -> Vec<Vec<char>> {
     let input = "input/input.txt";
     let contents = fs::read_to_string(input).unwrap();
 
     let grid: Vec<Vec<char>> = contents.lines().map(
         |x| x.chars().collect()
     ).collect();
+    grid
+}
 
+fn risky_slope(grid: &Vec<Vec<char>>, right: usize, down: usize) -> usize {
     let mut y: usize = 0;
     let mut trees: usize = 0;
 
-    grid.iter().for_each(|x| {
-        //println!("{}", x[y % x.len()]);
-        if x[y % x.len()] == '#' {
-            trees = trees + 1
+    grid.iter().enumerate().filter_map(|(i,x)| {
+        if i % down == 0 {
+            Some(x)
+        } else {
+            None
         }
-        y += 3
+    }).for_each(|x| {
+        if x[y % x.len()] == '#' {
+            trees += 1
+        }
+        y += right
     });
+    trees
+}
 
-    //println!("{:#?}", grid);
-    println!("{:#?}", trees);
+fn day_03_1() {
+    let grid = parse_input();
+    let trees = risky_slope(&grid, 3, 1);
+
+    println!("part1: {:#?}", trees);
+}
+
+fn day_03_2() {
+    let grid = parse_input();
+
+    let mut trees = risky_slope(&grid, 1, 1);
+    trees *= risky_slope(&grid, 3, 1);
+    trees *= risky_slope(&grid, 5, 1);
+    trees *= risky_slope(&grid, 7, 1);
+    trees *= risky_slope(&grid, 1, 2);
+
+    println!("part2: {:#?}", trees);
 }
 
 fn main() {
     day_03_1();
-    println!("Hello, world!");
+    day_03_2();
 }
