0917a6e84580049577fa1eabf092c6295dcb2703
Florian Vallée <florian@vallee.xyz>
2020-12-07 21:18:18 +0100

    day01: alternative implementation using itertools

--- src/day01.rs
+++ src/day01.rs
@@ -1,32 +1,50 @@
+use itertools::Itertools;
+
 #[aoc_generator(day1)]
 pub fn generator(input: &str) -> Vec<u32> {
     input.lines().map(|x| x.parse().unwrap()).collect()
 }
 
-#[aoc(day1, part1)] 
+#[aoc(day1, part1)]
 pub fn part1(expenses: &Vec<u32>) -> u32 {
-    let mut solution: u32 = 0;
-    for n1 in expenses {
-        for n2 in expenses {
+    for (i, n1) in expenses.iter().enumerate() {
+        for n2 in expenses.iter().skip(i + 1) {
             if n1 + n2 == 2020 {
-                solution = n1 * n2;
+                return n1 * n2;
             }
         }
     }
-    solution
+    return 0
 }
 
-#[aoc(day1, part2)] 
+#[aoc(day1, part2)]
 pub fn part2(expenses: &Vec<u32>) -> u32 {
-    let mut solution: u32 = 0;
-    for n1 in expenses {
-        for n2 in expenses {
-            for n3 in expenses {
+    for (i, n1) in expenses.iter().enumerate() {
+        for (j, n2) in expenses.iter().enumerate().skip(i + 1) {
+            for n3 in expenses.iter().skip(j) {
                 if n1 + n2 + n3 == 2020 {
-                    solution = n1 * n2 * n3
+                    return n1 * n2 * n3
                 }
             }
         }
     }
-    solution
+    return 0
+}
+
+#[aoc(day1, part1, itertools)]
+pub fn part1_iter(expenses: &Vec<u32>) -> u32 {
+    expenses.iter().tuple_combinations()
+                   .filter(|(&n1,&n2)| n1 + n2 == 2020)
+                   .map(|(n1, n2)| n1 * n2)
+                   .next()
+                   .unwrap()
+}
+
+#[aoc(day1, part2, itertools)]
+pub fn part2_iter(expenses: &Vec<u32>) -> u32 {
+    expenses.iter().tuple_combinations()
+                   .filter(|(&n1,&n2,&n3)| n1 + n2 + n3 == 2020)
+                   .map(|(&n1,&n2,&n3)| n1 * n2 * n3)
+                   .next()
+                   .unwrap()
 }
