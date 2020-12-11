462297daccc9e5ddf65fb348932c160cac637b96
Florian Vallée <florian@vallee.xyz>
2020-12-11 14:46:36 +0100

    day11: part2

--- src/day11.rs
+++ src/day11.rs
@@ -1,22 +1,24 @@
 use std::collections::HashMap;
 
+type Pt = (isize, isize);
+
 #[derive(Clone)]
 pub struct Floor {
-    plan: HashMap<(usize, usize), char>,
-    width: usize,
-    height: usize
+    plan: HashMap<Pt, char>,
+    width: isize,
+    height: isize
 }
 
 impl Floor {
     fn new(input: &str) -> Floor {
-        let mut plan: HashMap<(usize, usize), char> = HashMap::new();
-        let height = input.lines().count();
-        let width = input.lines().next().unwrap().chars().count();
+        let mut plan: HashMap<Pt, char> = HashMap::new();
+        let height = input.lines().count() as isize;
+        let width = input.lines().next().unwrap().chars().count() as isize;
 
         input.lines()
              .enumerate()
              .for_each(|(y, line)|
-                line.chars().enumerate().for_each(|(x, c)| {plan.insert((x,y), c); ()})
+                line.chars().enumerate().for_each(|(x, c)| {plan.insert((x as isize,y as isize), c); ()})
              );
         Floor {
             plan,
@@ -34,24 +36,55 @@ impl Floor {
         }
     }
 
-    fn neighbours(&self, (x, y): (usize, usize)) -> impl Iterator<Item=(usize, usize)> {
+    fn neighbours(&self, (x, y): Pt) -> impl Iterator<Item=Pt> {
         let width = self.width;
         let height = self.height;
         (-1..=1).flat_map(move |dx|
-                    (-1..=1).map(move |dy| (x as isize + dx, y as isize + dy))
+                    (-1..=1).map(move |dy| (x + dx, y + dy))
                )
                .filter(move |(c1, c2)| {
-                    (0..width as isize).contains(c1)
-                    && (0..height as isize).contains(c2)
-                    && (*c1, *c2) != (x as isize,y as isize)
+                    (0..width).contains(c1)
+                    && (0..height).contains(c2)
+                    && (*c1, *c2) != (x,y)
                })
-               .map(|(c1, c2)| (c1 as usize, c2 as usize))
+               .map(|(c1, c2)| (c1, c2))
                //.map(|c| {println!("{:#?}", c); c})
     }
 
+    fn contains(&self, (x,y): Pt) -> bool {
+        (0..self.width as isize).contains(&x)
+            && (0..self.height as isize).contains(&y)
+    }
+
+    fn add((x,y): Pt, (dx, dy): Pt) -> Pt {
+        (x + dx, y + dy)
+    }
+
     /* Could this return an iterator ? (borrows self ?) */
-    fn neighours_values(&self, coords: (usize, usize)) -> Vec<char> {
+    fn neighours_values(&self, coords: Pt) -> Vec<char> {
         self.neighbours(coords).map(|xy| self.plan[&xy]).collect()
+    }
+
+    fn seats(&self, point: Pt) -> Vec<char> {
+        let patterns = (-1..=1).flat_map(|x| (-1..=1).map(move |y| (x,y)));
+
+        patterns.filter(|pt| *pt != (0, 0))
+                .map(|direction| {
+            let mut seen: char = '.';
+            let mut cursor = point;
+            loop {
+                cursor = Floor::add(cursor, direction);
+                //println!("{:#?} {:#?}", cursor, direction);
+                if !self.contains(cursor) {
+                    break
+                }
+                seen = self.plan[&cursor];
+                if seen != '.' {
+                    break
+                }
+            }
+            seen
+        }).collect()
     }
 }
 
@@ -85,6 +118,38 @@ fn part1(floor: &Floor) -> usize {
         }).collect();
 
         //new_floor.debug();
+
+        let next_count = prev_floor.plan.values().filter(|&c| *c == '#').count();
+        if next_count == count {
+            break
+        }
+        prev_floor = new_floor;
+        count = next_count
+    }
+    count
+}
+
+#[aoc(day11, part2)]
+fn part2(floor: &Floor) -> usize {
+    let mut prev_floor = floor.clone();
+    let mut count = 1;
+
+    loop {
+        let mut new_floor = prev_floor.clone();
+        new_floor.plan = prev_floor.plan.iter().map(|(&k,&v)| {
+            if v == 'L' && prev_floor.seats(k)
+                                     .iter()
+                                     .all(|&x| x == 'L' || x == '.') {
+                (k, '#')
+            } else if v == '#' && prev_floor.seats(k)
+                                            .iter()
+                                            .filter(|&x| *x == '#')
+                                            .count() >= 5 {
+                (k, 'L')
+            } else {
+                (k, v)
+            }
+        }).collect();
 
         let next_count = prev_floor.plan.values().filter(|&c| *c == '#').count();
         if next_count == count {
